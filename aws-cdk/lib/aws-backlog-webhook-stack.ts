63b0459973ddaf5f0b604d7529f049e97d753a18
Lê Khải Hoàn <khaihoan2711@gmail.com>
2024-12-09 06:42:55 +0700

    Add Lambda handler

--- aws-cdk/lib/aws-backlog-webhook-stack.ts
+++ aws-cdk/lib/aws-backlog-webhook-stack.ts
@@ -1,17 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import path = require('path');
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsBacklogWebhookStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Create an IAM role for API Gateway to send messages to SQS
+    /** Create an IAM role for API Gateway to send messages to SQS */
     const webhookRole = new cdk.aws_iam.Role(this, 'IAMRole', {
       assumedBy: new cdk.aws_iam.ServicePrincipal('apigateway.amazonaws.com'),
     });
 
-    // Create SQS Queue
+    /** Create SQS Queue */
     const queue = new cdk.aws_sqs.Queue(this, 'SQSQueue', {
       visibilityTimeout: cdk.Duration.seconds(300),
       fifo: true, // FIFO must be enabled to preserve the order that webhook handler will clone the source code
@@ -30,7 +31,7 @@ export class AwsBacklogWebhookStack extends cdk.Stack {
       }
     });
 
-    // Create REST API
+    /** Create REST API */
     const restApi = new cdk.aws_apigateway.RestApi(this, 'RestApi', {
       restApiName: 'BacklogWebhook',
       description: 'Backlog Webhook Rest API',
@@ -45,10 +46,10 @@ export class AwsBacklogWebhookStack extends cdk.Stack {
       }
     });
 
-    // Grant permissions to send messages to the queue
+    /** Grant permissions to send messages to the queue */
     queue.grantSendMessages(webhookRole);
 
-    // Create an integration to send messages to SQS
+    /** Create an integration to send messages to SQS */
     const sqsIntegration = new cdk.aws_apigateway.AwsIntegration({
       service: 'sqs',
       path: `${cdk.Stack.of(this).account}/${queue.queueName}`,
@@ -70,7 +71,7 @@ export class AwsBacklogWebhookStack extends cdk.Stack {
             'MessageGroupId=$projectKey$concat$repositoryName$concat$gitRef',
             'MessageBody=$util.urlEncode($input.body)'
           ].join('&')
-        },        
+        },
         integrationResponses: [
           {
             statusCode: '200',
@@ -96,7 +97,7 @@ export class AwsBacklogWebhookStack extends cdk.Stack {
       }
     });
 
-    // Add a resource and method to the API
+    /** Add a resource and method to the API */
     const webhook = restApi.root
     webhook.addMethod('POST', sqsIntegration, {
       methodResponses: [
@@ -120,8 +121,8 @@ export class AwsBacklogWebhookStack extends cdk.Stack {
         }
       ]
     });
-    
-    // Add S3 Bucket for Lambda to archive source code
+
+    /** Add S3 Bucket for Lambda to archive source code */
     const bucket = new cdk.aws_s3.Bucket(this, 'SourceArchive', {
       versioned: true,
       blockPublicAccess: cdk.aws_s3.BlockPublicAccess.BLOCK_ALL,
@@ -135,7 +136,7 @@ export class AwsBacklogWebhookStack extends cdk.Stack {
       ]
     });
 
-    // Add DynamoDb table for Lambda to store webhook payload, S3 source version
+    /** Add DynamoDb table for Lambda to store webhook payload, S3 source version */
     const sourceInfoTable = new cdk.aws_dynamodb.Table(this, 'SourceInfo', {
       partitionKey: {
         name: 'S3_VersionId',
@@ -143,6 +144,18 @@ export class AwsBacklogWebhookStack extends cdk.Stack {
       },
       billingMode: cdk.aws_dynamodb.BillingMode.PAY_PER_REQUEST,
       timeToLiveAttribute: 'TTL'
+    });
+
+    /** Create a Lambda function from ../../webhook-handler using DockerImageFunction */
+    const handler = new cdk.aws_lambda.DockerImageFunction(this, 'LambdaHandler', {
+      code: cdk.aws_lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../../webhook-handler')),
+      description: 'From webhook payload, retrieve repository information, clone source and archive it to S3',
+      timeout: cdk.Duration.seconds(120),
+      tracing: cdk.aws_lambda.Tracing.ACTIVE,
+      environment: {
+        'S3_BUCKET': bucket.bucketName,
+        'DYNAMODB_TABLE': sourceInfoTable.tableName
+      }
     });
   }
 }
